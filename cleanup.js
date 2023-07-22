eabfc9474d6e2e5d39d49a133d3bedee763bba08
Kshitiz <kshitizagrawal@outlook.com>
2023-07-22 11:30:45 +0530

    fix

--- cleanup.js
+++ cleanup.js
@@ -5,20 +5,19 @@ const DOWNLOADS_DIRECTORY = "./downloads"
 const ZIPS_DIRECTORY = "./zips";
 
 const NOW = new Date();
-const DOWNLOADS = fs.readdirSync(DOWNLOADS_DIRECTORY)
-const ZIPS = fs.readdirSync(ZIPS_DIRECTORY)
 
-dirItemsRemove(DOWNLOADS, DOWNLOADS_DIRECTORY);
-dirItemsRemove(ZIPS, ZIPS_DIRECTORY)
+dirItemsRemove(DOWNLOADS_DIRECTORY);
+dirItemsRemove(ZIPS_DIRECTORY)
 
-function dirItemsRemove(DIRECTORY, DIRECTORY_PATH) {
+function dirItemsRemove(DIRECTORY_PATH) {
+    const DIRECTORY = fs.readdirSync(DIRECTORY_PATH);
     DIRECTORY.map(file => {
         const filePath = path.join(DIRECTORY_PATH, file)
 
         const stats = fs.statSync(filePath)
         const createdAt = stats.ctime;
 
-        if (NOW.getTime() - createdAt.getTime() > 30 * 24 * 60 * 60 * 1000) {
+        if (NOW.getTime() - createdAt.getTime() > 15 * 24 * 60 * 60 * 1000) {
             fs.rmSync(filePath, { recursive: true, force: true });
         }
     })
